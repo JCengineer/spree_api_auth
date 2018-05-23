49ad4a748b1877970cbe52b069cfa3c151f53132
matjemeisje <matje@taalmonsters.nl>
2018-05-23 14:47:00 +0200

    Fixed folder structure and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Spree::Core::Engine.add_routes do
   namespace :api, defaults: {format: 'json'} do
-    resource :users do
-      post :sign_up
-      post :sign_in
+    namespace :v1 do
+      post '/sign_up', action: :sign_up, controller: 'users'
+      post '/sign_in', action: :sign_in, controller: 'users'
     end
+    # resource :users do
+    #   post :sign_up
+    #   post :sign_in
+    # end
   end
 end
